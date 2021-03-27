d093ab8c43cb12a887139aff9c788f0d27e67572
shashidhar <raoshashidhar.3@gmail.com>
2021-03-27 18:56:29 -0400

    agent added
    Signed-off-by: shashidhar <raoshashidhar.3@gmail.com>

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,20 +1,21 @@
-def call(string component)
-pipeline {
-    agent any
-    stages {
-        stage('SonarQube') {
-            steps{
-                sh "sonar-scanner \
+def call(String component) {
+    pipeline {
+        agent any
+        stages {
+            stage('SonarQube') {
+                steps {
+                    sh "sonar-scanner \
             -Dsonar.projectKey=${component} \
             -Dsonar.sources=. \
             -Dsonar.host.url=http://100.24.117.168:9000 \
             -Dsonar.login=f8aaffb2b8ffd2f27270205c5775c20654b97530"
+                }
+                stage('SonarQube status') {
+                    steps {
+                        sh "sonar-quality-gate.sh admin password123 100.24.117.168 ${component}"
+                    }
+                }
             }
-        stage('SonarQube status') {
-            steps {
-                sh "sonar-quality-gate.sh admin password123 100.24.117.168 ${component}"
-            }
-        }
         }
     }
 }
